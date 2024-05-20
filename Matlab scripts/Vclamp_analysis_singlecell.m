48a88920a7d73ef30087cb5c55410845750e8d47
“Sayaka <saya1113saya@gmail.com>
2024-05-20 15:14:07 -0400

    May 20 3pm

--- Matlab scripts/Vclamp_analysis_singlecell.m
+++ Matlab scripts/Vclamp_analysis_singlecell.m
@@ -9,7 +9,7 @@ function Vclamp_analysis_singlecell(filename, sweepstoanalyze, starttime, endtim
 %endtime = time (in ms) at which region of analysis ends
 
 %% edit from here
-myVarnames = {'sweep number', 'duration(ms)', 'amplitude(pA)', 'min_value(pA)', 'synaptic_charge(pA*ms)'};
+myVarnames = {'sweep number', 'numSignals', 'avg_signal_duration(ms)', 'amplitude(pA)', 'min_value(pA)', 'synaptic_charge(pA*ms)'};
 
 multipleVariablesTable= zeros(0,size(myVarnames, 2));
 multipleVariablesRow = zeros(0,size(myVarnames, 2));
@@ -17,15 +17,6 @@ T= array2table(multipleVariablesTable, 'VariableNames', myVarnames); %stores inf
 
 
 %%%%%%%%% EXTRACT AND CLEAN DATA %%%%%%%%
-
-[dataallsweeps, si, h] =abf2load(filename1); %get si and h values from this abf file
-
-si_actual = 1e-6 * si; %in sec. original si is in usec
-
-cleaneddata = remove_transient(dataallsweeps); %remove transients
-
-b = 1;
-
 
 %use Gunay's function to load data. Cengiz Gunay (2024). loadVclampAbf (https://www.mathworks.com/matlabcentral/fileexchange/29018-loadvclampabf), MATLAB Central File Exchange. Retrieved March 23, 2024.
 %   time: Time vector for measurements [ms],
@@ -41,23 +32,50 @@ b = 1;
 data = data_i*1e-3; %includes all sweeps,converted pA->nA
 numsweeps = size(data_i,2); %get number of sweeps in data
 
-sweeps_to_analyze = 1:numsweeps; %specify the sweeps to analyze! 1:numsweeps to analyze all sweeps in file.
+if(isempty(sweepstoanalyze))
+    sweeps_to_analyze = 1:numsweeps; %specify the sweeps to analyze! 1:numsweeps to analyze all sweeps in file.
+else
+    sweeps_to_analyze = sweepstoanalyze;
+end
 
 synaptic_charge = zeros(length(numsweeps)); %array to store the total synaptic charge per sweep. aka area under curve for each sweep
 
-for i = 1:numel(sweepstoanalyze)
-    sweepnumber = sweepstoanalyze(i);
-    sweep_data = data(:,sweepnumber); %trace for the sweep to analyze
+for i = 1:numel(sweeps_to_analyze)
+    sweepnumber = sweeps_to_analyze(i);
+    sweep_data_whole = data(:,sweepnumber); %trace for the sweep to analyze
    
     %clean data
-    [sweep_data,timenew] = remove_transient(sweep_data,time);
-    sweep_data = detrend(sweep_data, 1) + sweep_data(1); %Correct baseline - detrend shifts baseline to 0
+    [sweep_data,timenew] = remove_transient(sweep_data_whole,time);
+    %sweep_data = detrend(sweep_data, 1) + sweep_data(1); %Correct baseline - detrend shifts baseline to 0. TODO: maybe don't include.
+    
+    %extract the region of interest
+    starttime_originalIdx = find(time==starttime); %convert start time in ms to index in sweep_data_whole
+    endtime_originalIdx = find(time == endtime); %endtime in ms to index in sweep_data_whole
+    transient_duration = 2643; %TODO: +/- 1? test these vals if it gives error
+    
+    starttime_newIdx = starttime_originalIdx-transient_duration; %start time on data with transient removed
+    endtime_newIdx = endtime_originalIdx - transient_duration; %end time on data with transient removed
+    baseline_current = sweep_data(1); 
 
-    %analyze region
-    synaptic_charge(i) = trapz(sweep_data); %calculate synaptic charge (area under curve) for this sweep (pC). abs() can be used to make all negatives positive
+    sweep_data_roi = sweep_data(starttime_newIdx:endtime_newIdx); %region of interest to analyze
+    
+
+    %calculate synaptic charge for the whole regionof interest
    
+    synaptic_charge(i) = trapz(sweep_data_roi) - trapz(baseline_current); %calculate synaptic charge (area under curve) for this sweep (pC). abs() can be used to make all negatives positive
+
+    % detect peaks in both directions
+    [pks, pklocs] = findpeaks(sweep_data_roi); %outward current peaks. gets indexes
+    [troughs,trlocs] = findpeaks(-sweep_data_roi); %inward current peaks. gets indexes
+
+    %TODO: find all pts where signal reaches baseline (0) before and after
+    %each peak to calculate AuC and duration for each signal
+
+
+
+
     %put into table
-    multipleVariablesRow1= [sweepnumber, synaptic_charge(i)];
+    multipleVariablesRow1= [sweepnumber, duration, synaptic_charge(i)];
    
     M1= array2table(multipleVariablesRow1, 'VariableNames', myVarnames1);
     T1 = [T1; M1]; %final table
